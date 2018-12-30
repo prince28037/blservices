00de4575360609d25c74837ea6c438262782d01a
Toki EIS <35416718+Toki-EIS@users.noreply.github.com>
2018-12-30 22:24:53 +0600

    mca calc

--- resources/views/calc/mca.blade.php
+++ resources/views/calc/mca.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Bare - Start Bootstrap Template</title>
+    <title>Business Loans Services - Merchant Cash Advance</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('public/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -20,7 +20,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="#">Business Loans</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -49,19 +49,65 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12 text-center">
-            <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
-            <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
-            <ul class="list-unstyled">
-                <li>Bootstrap 4.1.3</li>
-                <li>jQuery 3.3.1</li>
-            </ul>
+            <h1 class="mt-5">Merchant Cash Advance Calculator</h1>
+            <p class="lead">Enter your credentials for precise calculation</p>
         </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-header text-center">
+                    MCA Loan Calculator
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label>Advance Amount</label>
+                        <input class="form-control" type="text" id="ad-amount">
+                    </div>
+                    <div class="form-group">
+                        <label>Factor Rate</label>
+                        <input class="form-control" type="text" id="factor">
+                    </div>
+                    <div class="form-group">
+                        <label>Monthly Credit Card Sales</label>
+                        <input class="form-control" type="text" id="mccs">
+                    </div>
+                    <div class="form-group">
+                        <label>Credit Card Sales Purchased</label>
+                        <input class="form-control" type="text" id="ccsp">
+                    </div>
+                    <div class="form-group">
+                        <label>Origination Fee</label>
+                        <input class="form-control" type="text" id="of">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-info btn-lg btn-block" id="hishab">Calculate</button>
+                    </div>
+                </div>
+                <div class="card-footer text-center">
+                    <p class="small text-info">Powered by <a href="#">Xerotic Inc.</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4"></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12" id="calc"></div>
     </div>
 </div>
 
 <!-- Bootstrap core JavaScript -->
 <script src="{{ asset('public/assets/vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script>
+    $(document).ready(function(){
+        $('#hishab').on('click', function(e){
+            var html = '<h3>Base Calculation</h3>';
+            $('#calc').html(html);
+        });
+    });
+</script>
 
 </body>
 
