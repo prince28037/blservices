a46cb5c14dc6159f3b4a5c72c67da01edd4277a9
prince28037 <hm.shahriar.parvez@gmail.com>
2019-01-01 23:14:00 +0800

    update agree.blade

--- resources/views/pdf/agree.blade.php
+++ resources/views/pdf/agree.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Agreement For The Purchase & Sale Of Future Receipts</title>
+    <title>Agreement For The Purchase & Sale of Future Receipts</title>
     <style>
         .page-break {
             page-break-after: always;
@@ -8,30 +8,132 @@
     </style>
 </head>
 <body>
+<div class="agree-p1">
 <img src="{{ base_path() }}/public/pdf/agree/page1.jpg" width="725">
+
+<p class="seller-name">{{ $seller-name }}</p>
+<p class="seller-dba">{{ $seller-dba }}</p>
+
+<p class="chk-corp">{{ $chk-corp }}</p>
+<p class="chk-llc">{{ $chk-llc }}</p>
+<p class="chk-part">{{ $chk-part }}</p>
+<p class="chk-ltd">{{ $chk-ltd }}</p>
+<p class="chk-llp">{{ $chk-llp }}</p>
+<p class="chk-sole">{{ $chk-sole }}</p>
+<p class="chk-other">{{ $chk-other }}</p>
+<p class="text-other">{{ $text-other }}</p>
+
+<p class="str-addr">{{ $str-addr }}</p>
+<p class="city1">{{ $city1 }}</p>
+<p class="state1">{{ $state1 }}</p>
+<p class="zip1">{{ $zip1 }}</p>
+
+
+<p class="mail-addr">{{ $mail-addr }}</p>
+<p class="city2">{{ $city2 }}</p>
+<p class="state2">{{ $state2 }}</p>
+<p class="zip2">{{ $zip2 }}</p>
+
+<p class="pri-name">{{ $pri-name }}</p>
+<p class="title">{{ $title }}</p>
+<p class="taxid">{{ $taxid }}</p>
+
+<p class="pur-price">{{ $pur-price }}</p>
+<p class="pur-amount">{{ $pur-amount }}</p>
+<p class="mon-sales">{{ $mon-sales }}</p>
+
+
+<p class="percentage">{{ $percentage }}</p>
+<p class="ori-fee">{{ $ori-fee }}</p>
+<p class="daily-amount">{{ $daily-amount }}</p>
+
+
+<p class="eff-date">{{ $eff-date }}</p>
+
+
+<p class="seller-name">{{ $seller-name }}</p>
+<p class="pri-name">{{ $pri-name }}</p>
+<p class="title">{{ $title }}</p>
+
+
+
+</div>
 <div class="page-break"></div>
+<div class="agree-p2">
 <img src="{{ base_path() }}/public/pdf/agree/page2.jpg" width="725">
+</div>
 <div class="page-break"></div>
+<div class="agree-p3">
 <img src="{{ base_path() }}/public/pdf/agree/page3.jpg" width="725">
+</div>
 <div class="page-break"></div>
+<div class="agree-p4">
 <img src="{{ base_path() }}/public/pdf/agree/page4.jpg" width="725">
+</div>
 <div class="page-break"></div>
+<div class="agree-p5">
 <img src="{{ base_path() }}/public/pdf/agree/page5.jpg" width="725">
+</div>
 <div class="page-break"></div>
+<div class="agree-p6">
 <img src="{{ base_path() }}/public/pdf/agree/page6.jpg" width="725">
+</div>
 <div class="page-break"></div>
+<div class="agree-p7">
 <img src="{{ base_path() }}/public/pdf/agree/page7.jpg" width="725">
+</div>
 <div class="page-break"></div>
+<div class="agree-p8">
 <img src="{{ base_path() }}/public/pdf/agree/page8.jpg" width="725">
+</div>
+
 <div class="page-break"></div>
+<div class="agree-p9">
 <img src="{{ base_path() }}/public/pdf/agree/page9.jpg" width="725">
+
+<p class="seller-name">{{ $seller-name }}</p>
+<p class="pri-name">{{ $pri-name }}</p>
+<p class="title">{{ $title }}</p>
+
+</div>
 <div class="page-break"></div>
+<div class="agree-p10">
 <img src="{{ base_path() }}/public/pdf/agree/page10.jpg" width="725">
+
+<p class="exhi-date">{{ $exhi-date }}</p>
+<p class="pri-name">{{ $pri-name }}</p>
+<p class="guarantor-name">{{ $guarantor-name }}</p>
+
+<p class="seller-name">{{ $seller-name }}</p>
+
+</div>
 <div class="page-break"></div>
+<div class="agree-p11">
 <img src="{{ base_path() }}/public/pdf/agree/page11.jpg" width="725">
+</div>
 <div class="page-break"></div>
+<div class="agree-p12">
 <img src="{{ base_path() }}/public/pdf/agree/page12.jpg" width="725">
+
+<p class="pri-name">{{ $pri-name }}</p>
+<p class="guarantor2">{{ $guarantor2 }}</p>
+
+
+</div>
 <div class="page-break"></div>
+<div class="agree-p13">
 <img src="{{ base_path() }}/public/pdf/agree/page13.jpg" width="725">
+
+<p class="seller-name">{{ $seller-name }}</p>
+<p class="bank-name">{{ $bank-name }}</p>
+<p class="routing">{{ $routing }}</p>
+<p class="chk-acc">{{ $chk-acc }}</p>
+
+<p class="seller-name">{{ $seller-name }}</p>
+<p class="pri-name">{{ $pri-name }}</p>
+<p class="title">{{ $title }}</p>
+<p class="taxid">{{ $taxid }}</p>
+<p class="date13">{{ $date13 }}</p>
+</div>
 </body>
 </html>
