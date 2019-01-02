a68778d4121d72204a8befcd55286a87637f494c
prince28037 <hm.shahriar.parvez@gmail.com>
2019-01-02 20:00:50 +0800

    Update bcaapplicationdk.blade.php

--- resources/views/pdf/bcaapplicationdk.blade.php
+++ resources/views/pdf/bcaapplicationdk.blade.php
@@ -10,7 +10,229 @@
             position: relative;
             font-family: Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif;
         }
-
+		
+		  .chk1 {
+            position: absolute;
+            top: 417px;
+            left: 73px;
+        }
+		
+		 .chk2 {
+            position: absolute;
+            top: 427px;
+            left: 73px;
+        }
+		
+		.legal-name {
+			position: absolute;
+            top: 19%;
+            left: 18%;
+			
+		}
+		
+		.dba-name {
+			position: absolute;
+            top: 19%;
+            left: 57%;
+			
+		}
+		
+		
+		.chk-corp {
+			position: absolute;
+            top: 22.5%;
+            left: 20.5%;
+			
+		}
+		.chk-llc {
+			position: absolute;
+            top: 22.5%;
+            left: 25.5%;
+			
+		}
+		.chk-solep {
+			position: absolute;
+            top: 22.5%;
+            left: 38.2%;
+			
+		}
+		.chk-part {
+			position: absolute;
+            top: 22.5%;
+            left: 47.5%;
+			
+		}
+		
+		.tax-id {
+			position: absolute;
+            top: 22.1%;
+            left: 58.5%;
+			
+		}
+		
+		.bis-date {
+			position: absolute;
+            top: 22.1%;
+            left: 81.5%;
+			
+		}
+		
+		.bis-phone {
+			position: absolute;
+            top: 25.5%;
+            left: 15%;
+			
+		}
+		.fax {
+			position: absolute;
+            top: 25.5%;
+            left: 38%;
+			
+		}
+		.website {
+			position: absolute;
+            top: 25.5%;
+            left: 60%;
+			
+		}
+		
+		.phy-addr {
+			position: absolute;
+            top: 28.1%;
+            left: 16%;
+			
+		}
+		.city {
+			position: absolute;
+            top: 28.1%;
+            left: 53%;
+			
+		}
+		.state {
+			position: absolute;
+            top: 28.1%;
+            left: 80.3%;
+			
+		}
+		.zip {
+			position: absolute;
+            top: 28.1%;
+            left: 88.3%;
+			
+		}
+		
+		.mail-addr {
+			position: absolute;
+            top: 30.5%;
+            left: 16%;
+			
+		}
+		.city2 {
+			position: absolute;
+            top: 30.5%;
+            left: 53%;
+			
+		}
+		.state2 {
+			position: absolute;
+            top: 30.5%;
+            left: 80.3%;
+			
+		}
+		.zip2 {
+			position: absolute;
+            top: 30.5%;
+            left: 88.3%;
+			
+		}
+		
+		.owner-name {
+			position: absolute;
+            top: 34.7%;
+            left: 10.5%;
+			
+		}
+		.industry-type {
+			position: absolute;
+            top: 34.8%;
+            left: 68.5%;
+			
+		}
+		
+		.sqft-office {
+			position: absolute;
+            top: 34.8%;
+            left: 76.8%;
+			
+		}
+		
+		.owner-addr{
+			position: absolute;
+            top: 37%;
+            left: 15%;
+			
+		}
+		
+		.city-owner {
+			position: absolute;
+            top: 37%;
+            left: 53%;
+			
+		}
+		.state-owner {
+			position: absolute;
+            top: 37%;
+            left: 80.3%;
+			
+		}
+		.zip-owner {
+			position: absolute;
+            top: 37%;
+            left: 88.3%;
+			
+		}
+		
+		
+		.owner-email {
+			position: absolute;
+            top: 39.3%;
+            left: 10%;
+			
+		}
+		.owner-mobile {
+			position: absolute;
+            top: 39.3%;
+            left: 54.6%;
+			
+		}
+		
+		.owner-dob {
+			position: absolute;
+            top: 41.6%;
+            left: 14%;
+			
+		}
+		.owner-ssn {
+			position: absolute;
+            top: 41.6%;
+            left: 59.3%;
+			
+		}
+		.industry-type {
+			position: absolute;
+            top: 46%;
+            left: 14%;
+			
+		}
+		.sqft-office {
+			position: absolute;
+            top: 46%;
+            left: 61%;
+			
+		}
+		
+		
+		
     </style>
 </head>
 <body>
@@ -18,24 +240,24 @@
 <div class="bca1">
 <img src="{{ base_path() }}/public/pdf/bcaa/page1.jpg" width="725">
     <p class="chk1">Test</p>
-    <p class="chk2"></p>
-
+    <p class="chk2">Test</p>
 </div>
 
 <div class="page-break"></div>
 <div class="bca2">
 <img src="{{ base_path() }}/public/pdf/bcaa/page2.jpg" width="725">
 
-    /* Business Cash Advance Application */
 
-    <p class="legal-name"></p>
+
+    <p class="legal-name">Test</p>
     <p class="dba-name">Test</p>
 
-    <p class="chk-corp">Test</p>
-    <p class="chk-llc">Test</p>
-    <p class="chk-solep">Test</p>
-    <p class="chk-part">Test</p>
-    <p class="tax-id">Test</p>
+    <p class="chk-corp">T</p>
+    <p class="chk-llc">T</p>
+    <p class="chk-solep">T</p>
+    <p class="chk-part">T</p>
+   
+   <p class="tax-id">Test</p>
     <p class="bis-date">Test</p>
 
     <p class="bis-phone">Test</p>
@@ -52,11 +274,11 @@
     <p class="state2">Test</p>
     <p class="zip2">Test</p>
 
-    /* Owner/Principal Information */
+
 
     <p class="owner-name">Test</p>
-    <p class="chk-rent">Test</p>
-    <p class="chk-own">Test</p>
+    <p class="chk-rent">T</p>
+    <p class="chk-own">T</p>
 
     <p class="owner-addr">Test</p>
     <p class="city-owner">Test</p>
@@ -69,7 +291,7 @@
     <p class="owner-dob">Test</p>
     <p class="owner-ssn">Test</p>
 
-    /* Business Information */
+  
 
     <p class="industry-type">Test</p>
     <p class="sqft-office">Test</p>
@@ -79,7 +301,7 @@
 
     <p class="landmor-info">Test</p>
 
-    /* Funding & Financial Information */
+  
 
     <p class="annual-sales">Test</p>
     <p class="requested-amount">Test</p>
@@ -89,7 +311,7 @@
 
     <p class="out-balance">Test</p>
 
-    /* Authorization Form */
+
 
     <p class="sign-auth">Test</p>
     <p class="print-name">Test</p>
