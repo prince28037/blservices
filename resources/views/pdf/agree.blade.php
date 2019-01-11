ba507fb76d0d218a1b9a6d45a4fc1d3457220601
prince28037 <hm.shahriar.parvez@gmail.com>
2019-01-12 05:39:51 +0800

    agree update

--- resources/views/pdf/agree.blade.php
+++ resources/views/pdf/agree.blade.php
@@ -12,6 +12,508 @@
         }
 
 
+ 		 .seller-name {
+            position: absolute;
+            top: 6.4%;
+            left: 20%;
+        }
+
+          .seller-dba {
+            position: absolute;
+            top: 6.25%;
+            left: 59%;
+        }
+
+        .chk-corp {
+            position: absolute;
+            top: 10%;
+            left: 23%;
+        }
+          .chk-llc {
+            position: absolute;
+            top: 10%;
+            left: 34.5%;
+        }
+          .chk-part {
+            position: absolute;
+            top: 10%;
+            left: 54.9%;
+        }
+
+  		.chk-ltd {
+            position: absolute;
+            top: 10%;
+            left: 65.8%;
+        }
+
+  		.chk-llp {
+            position: absolute;
+            top: 12.2%;
+            left: 22.8%;
+        }
+  		.chk-sole {
+            position: absolute;
+            top: 12.2%;
+            left: 44.5%;
+        }
+  		.chk-other {
+            position: absolute;
+            top: 12.2%;
+            left: 60.5%;
+        }
+  		.text-other {
+            position: absolute;
+            top: 12.05%;
+            left: 67%;
+        }
+
+
+
+
+ .str-addr {
+            position: absolute;
+            top: 15.45%;
+            left: 16.5%;
+        }
+
+ .city1 {
+            position: absolute;
+            top: 15.45%;
+            left: 50.9%;
+        }
+
+ .state1 {
+            position: absolute;
+            top: 15.45%;
+            left: 75.2%;
+        }
+
+ .zip1 {
+            position: absolute;
+            top: 15.45%;
+            left: 85.6%;
+        }
+
+
+
+.mail-addr {
+            position: absolute;
+            top: 19.1%;
+            left: 18%;
+        }
+
+ .city2 {
+            position: absolute;
+            top: 19.1%;
+            left: 50.9%;
+        }
+
+ .state2 {
+            position: absolute;
+            top: 19.1%;
+            left: 75.2%;
+        }
+
+ .zip2 {
+            position: absolute;
+            top: 19.1%;
+            left: 85.6%;
+        }
+
+
+
+
+
+.pri-name {
+            position: absolute;
+            top: 22.45%;
+            left: 21.6%;
+        }
+
+ .title {
+            position: absolute;
+            top: 22.45%;
+            left: 51.3%;
+        }
+
+ .taxid1 {
+            position: absolute;
+            top: 22.4%;
+            left: 81.5%;
+        }
+
+
+
+.pur-price {
+            position: absolute;
+            top: 28%;
+            left: 18%;
+        }
+
+ .pur-amount {
+            position: absolute;
+            top: 28%;
+            left: 45.5%;
+        }
+
+ .mon-sales {
+            position: absolute;
+            top: 28%;
+            left: 77.7%;
+        }
+
+
+
+		.pur-price {
+            position: absolute;
+            top: 27.5%;
+            left: 18%;
+        }
+
+ 		.pur-amount {
+            position: absolute;
+            top: 27.5%;
+            left: 45.5%;
+        }
+
+ 		.mon-sales {
+            position: absolute;
+            top: 27.5%;
+            left: 77.7%;
+        }
+
+
+
+        .percentage {
+            position: absolute;
+            top: 31.55%;
+            left: 20.3%;
+        }
+
+ 		.ori-fee {
+            position: absolute;
+            top: 31.65%;
+            left: 35.1%;
+        }
+
+ 		.daily-amount {
+            position: absolute;
+            top: 31.65%;
+            left: 55%;
+        }
+
+ 
+ 		.ini-amount {
+            position: absolute;
+            top: 35%;
+            left: 21.3%;
+        }
+
+		.eff-date {
+            position: absolute;
+            top: 40.1%;
+            left: 12%;
+        }
+
+
+
+
+
+        .sellerr-name {
+            position: absolute;
+            top: 72.3%;
+            left: 10.2%;
+        }
+        .prii-name {
+            position: absolute;
+            top: 74.7%;
+            left: 66.5%;
+        }
+        .titlee {
+            position: absolute;
+            top: 78%;
+            left: 8.2%;
+        }
+
+
+
+
+ 		.ri1 {
+            position: absolute;
+            top: 91%;
+            left: 87%;
+        }
+        .ow1 {
+            position: absolute;
+            top: 93.2%;
+            left: 87%;
+        }
+
+
+
+
+		.ri2 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow2 {
+            position: absolute;
+            top: 93.1%;
+            left: 87%;
+        }
+
+
+
+        .ri3 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow3 {
+            position: absolute;
+            top: 93.1%;
+            left: 87%;
+        }
+
+
+
+		.ri4 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow4 {
+            position: absolute;
+            top: 93.1%;
+            left: 87%;
+        }
+
+
+        .ri5 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow5 {
+            position: absolute;
+            top: 93.1%;
+            left: 87%;
+        }
+
+
+ 		.ri6 {
+            position: absolute;
+            top: 91%;
+            left: 87%;
+        }
+        .ow6 {
+            position: absolute;
+            top: 93.3%;
+            left: 87%;
+        }
+
+		.ri7 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow7 {
+            position: absolute;
+            top: 93.05%;
+            left: 87%;
+        }
+
+
+		.ri8 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow8 {
+            position: absolute;
+            top: 93.1%;
+            left: 87%;
+        }
+
+
+		.seller9-name {
+            position: absolute;
+            top: 42.4%;
+            left: 44.5%;
+        }
+
+
+		.pri9-name {
+            position: absolute;
+            top: 51.5%;
+            left: 44.5%;
+        }
+        .title9 {
+            position: absolute;
+            top: 55.5%;
+            left: 44.5%;
+        }
+
+        .ri9 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow9 {
+            position: absolute;
+            top: 93%;
+            left: 87%;
+        }
+
+		
+
+		.exhi-date {
+            position: absolute;
+            top: 12.65%;
+            left: 55.5%;
+        }
+		.pri10-name {
+            position: absolute;
+            top: 15.7%;
+            left: 8%;
+        }
+        .guarantor-name {
+            position: absolute;
+            top: 15.7%;
+            left: 35%;
+        }
+         .seller10-name {
+            position: absolute;
+            top: 30.8%;
+            left: 29%;
+        }
+
+		.ri10 {
+            position: absolute;
+            top: 91%;
+            left: 87%;
+        }
+        .ow10 {
+            position: absolute;
+            top: 93.2%;
+            left: 87%;
+        }
+
+		
+
+
+		.ri11 {
+            position: absolute;
+            top: 91%;
+            left: 87%;
+        }
+        .ow11 {
+            position: absolute;
+            top: 93.2%;
+            left: 87%;
+        }
+
+
+
+
+		.pri12-name {
+            position: absolute;
+            top: 67.2%;
+            left: 54.9%;
+        }
+		.guarantor12 {
+            position: absolute;
+            top: 79%;
+            left: 54.9%;
+        }
+        .guarantor122 {
+            position: absolute;
+            top: 83.4%;
+            left: 54.9%;
+        }
+
+		.ri12 {
+            position: absolute;
+            top: 91%;
+            left: 87%;
+        }
+        .ow12 {
+            position: absolute;
+            top: 93.15%;
+            left: 87%;
+        }
+
+
+		.seller13-name {
+            position: absolute;
+            top: 11.5%;
+            left: 7%;
+        }
+
+        .exact-name {
+            position: absolute;
+            top: 44%;
+            left: 47.5%;
+        }
+
+        .bank-name {
+            position: absolute;
+            top: 47.5%;
+            left: 47.5%;
+        }
+
+		.routing {
+            position: absolute;
+            top: 50.5%;
+            left: 47.5%;
+        }
+        .chk-acc {
+            position: absolute;
+            top: 53.5%;
+            left: 47.5%;
+        }
+		.seller132-name {
+            position: absolute;
+            top: 62.7%;
+            left: 47.5%;
+        }
+		.pri13-name {
+            position: absolute;
+            top: 68.1%;
+            left: 47.5%;
+        }
+		.title13 {
+            position: absolute;
+            top: 71%;
+            left: 47.5%;
+        }
+		.taxid {
+            position: absolute;
+            top: 74.1%;
+            left: 47.5%;
+        }
+		.date13 {
+            position: absolute;
+            top: 77.2%;
+            left: 47.5%;
+        }
+
+
+
+		.ri13 {
+            position: absolute;
+            top: 90.9%;
+            left: 87%;
+        }
+        .ow13 {
+            position: absolute;
+            top: 93.1%;
+            left: 87%;
+        }
+
+		
+
 
     </style>
 </head>
@@ -20,94 +522,134 @@
 <div class="agree-p1">
 <img src="{{ base_path() }}/public/pdf/agree/page1.jpg" width="725">
 
-	<p class="seller-name">Test</p>
-	<p class="seller-dba">Test</p>
+	<div class="seller-name">Test</div>
+	<div class="seller-dba">Test</div>
 
-	<p class="chk-corp">Test</p>
-	<p class="chk-llc">Test</p>
-	<p class="chk-part">Test</p>
-	<p class="chk-ltd">Test</p>
-	<p class="chk-llp">Test</p>
-	<p class="chk-sole">Test</p>
-	<p class="chk-other">Test</p>
-	<p class="text-other">Test</p>
+	<div class="chk-corp">T</div>
+	<div class="chk-llc">T</div>
+	<div class="chk-part">T</div>
+	<div class="chk-ltd">T</div>
+	<div class="chk-llp">T</div>
+	<div class="chk-sole">T</div>
+	<div class="chk-other">T</div>
+	<div class="text-other">T</div>
 
-	<p class="str-addr">Test</p>
-	<p class="city1">Test</p>
-	<p class="state1">Test</p>
-	<p class="zip1">Test</p>
-
-
-	<p class="mail-addr">Test</p>
-	<p class="city2">Test</p>
-	<p class="state2">Test</p>
-	<p class="zip2">Test</p>
-
-	<p class="pri-name">Test</p>
-	<p class="title">Test</p>
-	<p class="taxid">Test</p>
-
-	<p class="pur-price">Test</p>
-	<p class="pur-amount">Test</p>
-	<p class="mon-sales">Test</p>
+	<div class="str-addr">Test</div>
+	<div class="city1">Test</div>
+	<div class="state1">Test</div>
+	<div class="zip1">Test</div>
 
 
-	<p class="percentage">Test</p>
-	<p class="ori-fee">Test</p>
-	<p class="daily-amount">Test</p>
+	<div class="mail-addr">Test</div>
+	<div class="city2">Test</div>
+	<div class="state2">Test</div>
+	<div class="zip2">Test</div>
+
+	<div class="pri-name">Test</div>
+	<div class="title">Test</div>
+	<div class="taxid1">Test</div>
+
+	<div class="pur-price">Test</div>
+	<div class="pur-amount">Test</div>
+	<div class="mon-sales">Test</div>
 
 
-	<p class="eff-date">Test</p>
+	<div class="percentage">Test</div>
+	<div class="ori-fee">Test</div>
+	<div class="daily-amount">Test</div>
+
+	<div class="ini-amount">Test</div>
+
+	<div class="eff-date">Test</div>
 
 
-	<p class="seller-name">Test</p>
-	<p class="pri-name">Test</p>
-	<p class="title">Test</p>
+	<div class="sellerr-name">Test</div>
+	<div class="prii-name">Test</div>
+	<div class="titlee">Test</div>
+
+	<div class="ri1">Testg</div>
+	<div class="ow1">Testg</div>
+
 
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p2">
 <img src="{{ base_path() }}/public/pdf/agree/page2.jpg" width="725">
+
+
+	<div class="ri2">Testg</div>
+	<div class="ow2">Testg</div>
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p3">
 <img src="{{ base_path() }}/public/pdf/agree/page3.jpg" width="725">
+
+
+	<div class="ri3">Testg</div>
+	<div class="ow3">Testg</div>
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p4">
 <img src="{{ base_path() }}/public/pdf/agree/page4.jpg" width="725">
+
+
+	<div class="ri4">Testg</div>
+	<div class="ow4">Testg</div>
+
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p5">
 <img src="{{ base_path() }}/public/pdf/agree/page5.jpg" width="725">
+
+
+	<div class="ri5">Testg</div>
+	<div class="ow5">Testg</div>
+
+
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p6">
 <img src="{{ base_path() }}/public/pdf/agree/page6.jpg" width="725">
+
+
+	<div class="ri6">Testg</div>
+	<div class="ow6">Testg</div>
+
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p7">
 <img src="{{ base_path() }}/public/pdf/agree/page7.jpg" width="725">
+
+	<div class="ri7">Testg</div>
+	<div class="ow7">Testg</div>
+
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p8">
 <img src="{{ base_path() }}/public/pdf/agree/page8.jpg" width="725">
+
+	<div class="ri8">Testg</div>
+	<div class="ow8">Testg</div>
+
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p9">
 <img src="{{ base_path() }}/public/pdf/agree/page9.jpg" width="725">
 
-	<p class="seller-name">Test</p>
-	<p class="pri-name">Test</p>
-	<p class="title">Test</p>
+	<div class="seller9-name">Test</div>
+	<div class="pri9-name">Test</div>
+	<div class="title9">Test</div>
+
+	<div class="ri9">Testg</div>
+	<div class="ow9">Testg</div>
 
 </div>
 
@@ -115,25 +657,37 @@
 <div class="agree-p10">
 <img src="{{ base_path() }}/public/pdf/agree/page10.jpg" width="725">
 
-	<p class="exhi-date">Test</p>
-	<p class="pri-name">Test</p>
-	<p class="guarantor-name">Test</p>
+	<div class="exhi-date">Test</div>
+	<div class="pri10-name">Test</div>
+	<div class="guarantor-name">Test</div>
 
-	<p class="seller-name">Test</p>
+	<div class="seller10-name">Test</div>
+
+	<div class="ri10">Testg</div>
+	<div class="ow10">Testg</div>
 
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p11">
 <img src="{{ base_path() }}/public/pdf/agree/page11.jpg" width="725">
+
+	<div class="ri11">Testg</div>
+	<div class="ow11">Testg</div>
+
 </div>
 
 <div class="page-break"></div>
 <div class="agree-p12">
 <img src="{{ base_path() }}/public/pdf/agree/page12.jpg" width="725">
 
-	<p class="pri-name">Test</p>
-	<p class="guarantor2">Test</p>
+	<div class="pri12-name">Test</div>
+	<div class="guarantor12">Test</div>
+	
+	<div class="guarantor122">Test</div>
+
+	<div class="ri12">Testg</div>
+	<div class="ow12">Testg</div>
 
 </div>
 
@@ -141,16 +695,21 @@
 <div class="agree-p13">
 <img src="{{ base_path() }}/public/pdf/agree/page13.jpg" width="725">
 
-	<p class="seller-name">Test</p>
-	<p class="bank-name">Test</p>
-	<p class="routing">Test</p>
-	<p class="chk-acc">Test</p>
+	<div class="seller13-name">Test</div>
+	<div class="exact-name">Test</div>
+	<div class="bank-name">Test</div>
+	<div class="routing">Test</div>
+	<div class="chk-acc">Test</div>
 
-	<p class="seller-name">Test</p>
-	<p class="pri-name">Test</p>
-	<p class="title">Test</p>
-	<p class="taxid">Test</p>
-	<p class="date13">Test</p>
+	<div class="seller132-name">Test</div>
+	<div class="pri13-name">Test</div>
+	<div class="title13">Test</div>
+	<div class="taxid">Test</div>
+	<div class="date13">Test</div>
+
+
+	<div class="ri13">Testg</div>
+	<div class="ow13">Testg</div>
 	
 </div>
 </body>
