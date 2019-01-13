ea2869e076dcd7c8288c74986eb340d1bafee98d
prince28037 <hm.shahriar.parvez@gmail.com>
2019-01-14 03:56:08 +0800

    update rpa

--- resources/views/pdf/rpa.blade.php
+++ resources/views/pdf/rpa.blade.php
@@ -10,66 +10,572 @@
             position: relative;
             font-family: Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif;
         }
+
+		.eff-date {
+            position: absolute;
+            top: 20.4%;
+            left: 56.8%;
+        }
+
+        .ini1 {
+            position: absolute;
+            top: 87.5%;
+            left: 80%;
+        }
+
+        .ini12 {
+            position: absolute;
+            top: 90%;
+            left: 80%;
+        }
+
+
+
+ 		.ini2 {
+            position: absolute;
+            top: 85.2%;
+            left: 80%;
+        }
+
+        .ini122 {
+            position: absolute;
+            top: 87.6%;
+            left: 80%;
+        }
+
+
+		.ini3 {
+            position: absolute;
+            top: 85.2%;
+            left: 80%;
+        }
+
+        .ini132 {
+            position: absolute;
+            top: 87.6%;
+            left: 80%;
+        }
+
+
+
+		.refer-name {
+            position: absolute;
+            top: 40.8%;
+            left: 51.5%;
+        }
+
+        .refer-addr1 {
+            position: absolute;
+            top: 43.4%;
+            left: 51.5%;
+        }
+  		
+  		.refer-addr2 {
+            position: absolute;
+            top: 47%;
+            left: 51.5%;
+        }
+
+		.ini4 {
+            position: absolute;
+            top: 85.2%;
+            left: 80%;
+        }
+
+        .ini142 {
+            position: absolute;
+            top: 87.6%;
+            left: 80%;
+        }
+
+
+		.ini5 {
+            position: absolute;
+            top: 85.2%;
+            left: 80%;
+        }
+
+        .ini152 {
+            position: absolute;
+            top: 87.6%;
+            left: 80%;
+        }
+
+
+
+		.ini6 {
+            position: absolute;
+            top: 85.2%;
+            left: 80%;
+        }
+
+        .ini162 {
+            position: absolute;
+            top: 87.6%;
+            left: 80%;
+        }
+
+
+  		.refer7-name {
+            position: absolute;
+            top: 18%;
+            left: 67.5%;
+        }
+
+
+		.s-printname {
+            position: absolute;
+            top: 24.2%;
+            left: 25%;
+        }
+        .refer-printname {
+            position: absolute;
+            top: 24.2%;
+            left: 64.4%;
+        }
+
+        .s-title {
+            position: absolute;
+            top: 30.4%;
+            left: 20.2%;
+        }
+
+        .refer7-title {
+            position: absolute;
+            top: 30.4%;
+            left: 59%;
+        }
+
+         .s-date {
+            position: absolute;
+            top: 42.5%;
+            left: 24%;
+        }
+         .refer-date {
+            position: absolute;
+            top: 42.5%;
+            left: 63%;
+        }
+
+        .ini7 {
+            position: absolute;
+            top: 85.2%;
+            left: 80%;
+        }
+
+        .ini72 {
+            position: absolute;
+            top: 87.6%;
+            left: 80%;
+        }
+
+
+
+		.refer8-name {
+            position: absolute;
+            top: 11.5%;
+            left: 31.7%;
+        }
+
+        .chk-new {
+            position: absolute;
+            top: 18.8%;
+            left: 14.9%;
+        }
+		.chk-binfo {
+            position: absolute;
+            top: 18.8%;
+            left: 37.8%;
+        }
+
+        .chk-ach {
+            position: absolute;
+            top: 18.8%;
+            left: 60.35%;
+        }
+
+
+		.bname {
+            position: absolute;
+            top: 36.5%;
+            left: 15%;
+        }
+
+        .baddr {
+            position: absolute;
+            top: 36.5%;
+            left: 51%;
+        }
+
+		.bphone {
+            position: absolute;
+            top: 43.5%;
+            left: 15%;
+        }
+
+ 		.btype {
+            position: absolute;
+            top: 43.5%;
+            left: 51%;
+        }
+        .bacct {
+            position: absolute;
+            top: 50%;
+            left: 15%;
+        }
+	
+		.brout {
+            position: absolute;
+            top: 50%;
+            left: 51%;
+        }
+
+        .bdate {
+            position: absolute;
+            top: 56.5%;
+            left: 15%;
+        }
+
+
+ 		.ini8 {
+            position: absolute;
+            top: 85.2%;
+            left: 80%;
+        }
+
+        .ini82 {
+            position: absolute;
+            top: 87.6%;
+            left: 80%;
+        }
+
+
+
+		.legal-name {
+            position: absolute;
+            top: 18%;
+            left: 18.5%;
+        }
+
+        .dba-name {
+            position: absolute;
+            top: 18%;
+            left: 53%;
+        }
+
+
+		.contact {
+            position: absolute;
+            top: 20.7%;
+            left: 11%;
+        }
+
+        .phone {
+            position: absolute;
+            top: 20.7%;
+            left: 33%;
+        }
+
+ 		.email {
+            position: absolute;
+            top: 20.7%;
+            left: 50.1%;
+        }
+
+        .current-address {
+            position: absolute;
+            top: 23.3%;
+            left: 15.5%;
+        }
+
+        .city {
+            position: absolute;
+            top: 26.2%;
+            left: 8.7%;
+        }
+
+        .state {
+            position: absolute;
+            top: 26.2%;
+            left: 32.5%;
+        }
+        .zip {
+            position: absolute;
+            top: 26.2%;
+            left: 48.9%;
+        }
+
+         .taxid {
+            position: absolute;
+            top: 29%;
+            left: 10%;
+        }
+        .bisyear {
+            position: absolute;
+            top: 29%;
+            left: 38.9%;
+        }
+
+ 		.struct {
+            position: absolute;
+            top: 29%;
+            left: 55.9%;
+        }
+         .incorp {
+            position: absolute;
+            top: 29%;
+            left: 79.2%;
+        }
+		.deal-units {
+            position: absolute;
+            top: 31.7%;
+            left: 24%;
+        }
+
+        .deal-dollar {
+            position: absolute;
+            top: 31.7%;
+            left: 65.2%;
+        }
+
+
+		.tot-fund {
+            position: absolute;
+            top: 34.5%;
+            left: 24.4%;
+        }
+
+        .website {
+            position: absolute;
+            top: 34.5%;
+            left: 51.7%;
+        }
+
+
+        .pname {
+            position: absolute;
+            top: 39.9%;
+            left: 10%;
+        }
+
+        .pdob {
+            position: absolute;
+            top: 39.9%;
+            left: 49.8%;
+        }
+
+ 		.pcell {
+            position: absolute;
+            top: 42.5%;
+            left: 9%;
+        }
+
+        .pssn {
+            position: absolute;
+            top: 42.5%;
+            left: 49.6%;
+        }
+
+ 		.paddr {
+            position: absolute;
+            top: 45.1%;
+            left: 14.5%;
+        }
+		
+		.pcity {
+            position: absolute;
+            top: 48.1%;
+            left: 8.7%;
+        }
+        .pstate {
+            position: absolute;
+            top: 48.1%;
+            left: 32.5%;
+        }
+        .pzip {
+            position: absolute;
+            top: 48.1%;
+            left: 48.9%;
+        }
+
+		
+		.pemail {
+            position: absolute;
+            top: 50.9%;
+            left: 9.7%;
+        }
+        .pexp {
+            position: absolute;
+            top: 50.9%;
+            left: 62%;
+        }
+        .ptitle {
+            position: absolute;
+            top: 50.9%;
+            left: 69.7%;
+        }
+
+		.iname {
+            position: absolute;
+            top: 56.2%;
+            left: 15.2%;
+        }
+        .icontact {
+            position: absolute;
+            top: 58.9%;
+            left: 11%;
+        }
+        .iphone {
+            position: absolute;
+            top: 58.9%;
+            left: 33%;
+        }
+        .iemail {
+            position: absolute;
+            top: 58.9%;
+            left: 50.1%;
+        }
+        .icname {
+            position: absolute;
+            top: 61.7%;
+            left: 15.4%;
+        }
+
+		.iccontact {
+            position: absolute;
+            top: 64.4%;
+            left: 11%;
+        }
+
+        .icphone {
+            position: absolute;
+            top: 64.4%;
+            left: 33%;
+        }
+        .icemail {
+            position: absolute;
+            top: 64.4%;
+            left: 50.1%;
+        }
+        .past {
+            position: absolute;
+            top: 67.1%;
+            left: 35%;
+        }
+
+		.why {
+            position: absolute;
+            top: 67.1%;
+            left: 58%;
+        }
+
+		.hear {
+            position: absolute;
+            top: 69.8%;
+            left: 31%;
+        }
+
+		.idate {
+            position: absolute;
+            top: 88.2%;
+            left: 50%;
+        }
+
+
+
     </style>
 </head>
 <body>
 
 <div class="refer-p1">
 <img src="{{ base_path() }}/public/pdf/rpa/page1.jpg" width="725">
-	<p class="eff-date">Test</p>
+	<div class="eff-date">Test</div>
+	<div class="ini1">Test</div>
+	<div class="ini12">Test</div>
+
 
 </div>
 
 <div class="page-break"></div>
 <img src="{{ base_path() }}/public/pdf/rpa/page2.jpg" width="725">
+	
+	<div class="ini2">Test</div>
+	<div class="ini122">Test</div>
+
 
 <div class="page-break"></div>
 <img src="{{ base_path() }}/public/pdf/rpa/page3.jpg" width="725">
+	
+	<div class="ini3">Test</div>
+	<div class="ini132">Test</div>
 
 <div class="page-break"></div>
 
 <div class="refer-p4">
 <img src="{{ base_path() }}/public/pdf/rpa/page4.jpg" width="725">
-	<p class="refer-name">Test</p>
-	<p class="refer-addr">Test</p>
+	<div class="refer-name">Test</div>
+	<div class="refer-addr1">Test</div>
+	<div class="refer-addr2">Test</div>
+
+	<div class="ini4">Test</div>
+	<div class="ini142">Test</div>
 </div>
 
 <div class="page-break"></div>
 <img src="{{ base_path() }}/public/pdf/rpa/page5.jpg" width="725">
 
+	<div class="ini5">Test</div>
+	<div class="ini152">Test</div>
+
 <div class="page-break"></div>
 <img src="{{ base_path() }}/public/pdf/rpa/page6.jpg" width="725">
+
+
+	<div class="ini6">Test</div>
+	<div class="ini162">Test</div>
 
 <div class="page-break"></div>
 <div class="refer-p7">
 <img src="{{ base_path() }}/public/pdf/rpa/page7.jpg" width="725">
 
-	<p class="refer-name">Test</p>
+	<div class="refer7-name">Test</div>
 
-	<p class="s-printname">Test</p>
-	<p class="refer-printname">Test</p>
+	<div class="s-printname">Test</div>
+	<div class="refer-printname">Test</div>
 
-	<p class="s-title">Test</p>
-	<p class="refer-title">Test</p>
+	<div class="s-title">Test</div>
+	<div class="refer7-title">Test</div>
+
+	<div class="s-date">Test</div>
+	<div class="refer-date">Test</div>
+
+
+	<div class="ini7">Test</div>
+	<div class="ini72">Test</div>
+
 </div>
 
 <div class="page-break"></div>
 <div class="refer-p8">
 <img src="{{ base_path() }}/public/pdf/rpa/page8.jpg" width="725">
 
-	<p class="refer-name">Test</p>
-	<p class="chk-new">Test</p>
-	<p class="chk-binfo">Test</p>
-	<p class="chk-ach">Test</p>
+	<div class="refer8-name">Tggg</div>
+	<div class="chk-new">T</div>
+	<div class="chk-binfo">T</div>
+	<div class="chk-ach">T</div>
 
 
-	<p class="bname">Test</p>
-	<p class="baddr">Test</p>
-	<p class="bphone">Test</p>
-	<p class="btype">Test</p>
-	<p class="bacct">Test</p>
-	<p class="brout">Test</p>
-	<p class="bdate">Test</p>
+	<div class="bname">Test</div>
+	<div class="baddr">Test</div>
+	<div class="bphone">Test</div>
+	<div class="btype">Test</div>
+	<div class="bacct">Test</div>
+	<div class="brout">Test</div>
+	<div class="bdate">Test</div>
+
+
+	<div class="ini8">Test</div>
+	<div class="ini82">Test</div>
 
 </div>
 
@@ -77,53 +583,54 @@
 <div class="refer-p9">
 <img src="{{ base_path() }}/public/pdf/rpa/page9.jpg" width="725">
 
-/* Company Information */
-	<p class="legal-name">Test</p>
 
-	<p class="dba-name">Test</p>
+	<div class="legal-name">Test</div>
 
-	<p class="contact">Test</p>
-	<p class="phone">Test</p>
-	<p class="email">Test</p>
-	<p class="current-address">Test</p>
-	<p class="city">Test</p>
-	<p class="state">Test</p>
-	<p class="zip">Test</p>
-	<p class="taxid">Test</p>
-	<p class="bisyear">Test</p>
-	<p class="struct">Test</p>
-	<p class="incorp">Test</p>
-	<p class="deal-units">Test</p>
-	<p class="deal-dollar">Test</p>
-	<p class="tot-fund">Test</p>
-	<p class="website">Test</p>
+	<div class="dba-name">Test</div>
 
-/* Princial Information */
+	<div class="contact">Test</div>
+	<div class="phone">Test</div>
+	<div class="email">Test</div>
+	<div class="current-address">Test</div>
+	<div class="city">Test</div>
+	<div class="state">Test</div>
+	<div class="zip">Test</div>
+	<div class="taxid">Test</div>
+	<div class="bisyear">Test</div>
+	<div class="struct">Test</div>
+	<div class="incorp">Test</div>
+	<div class="deal-units">Test</div>
+	<div class="deal-dollar">Test</div>
+	<div class="tot-fund">Test</div>
+	<div class="website">Test</div>
 
-	<p class="pname">Test</p>
-	<p class="pdob">Test</p>
-	<p class="pcell">Test</p>
-	<p class="pssn">Test</p>
-	<p class="paddr">Test</p>
-	<p class="pcity">Test</p>
-	<p class="pstate">Test</p>
-	<p class="pzip">Test</p>
-	<p class="pemail">Test</p>
-	<p class="pexp">Test</p>
-	<p class="ptitle">Test</p>
 
-/* Industry Trade References */
-	<p class="iname">Test</p>
-	<p class="icontact">Test</p>
-	<p class="iphone">Test</p>
-	<p class="iemail">Test</p>
-	<p class="icname">Test</p>
-	<p class="icphone">Test</p>
-	<p class="icemail">Test</p>
-	<p class="past">Test</p>
-	<p class="why">Test</p>
-	<p class="hear">Test</p>
+	<div class="pname">Test</div>
+	<div class="pdob">Test</div>
+	<div class="pcell">Test</div>
+	<div class="pssn">Test</div>
+	<div class="paddr">Test</div>
+	<div class="pcity">Test</div>
+	<div class="pstate">Test</div>
+	<div class="pzip">Test</div>
+	<div class="pemail">Test</div>
+	<div class="pexp">Test</div>
+	<div class="ptitle">Test</div>
 
+
+	<div class="iname">Test</div>
+	<div class="icontact">Test</div>
+	<div class="iphone">Test</div>
+	<div class="iemail">Test</div>
+	<div class="icname">Test</div>
+	<div class="iccontact">Test</div>
+	<div class="icphone">Test</div>
+	<div class="icemail">Test</div>
+	<div class="past">Test</div>
+	<div class="why">Test</div>
+	<div class="hear">Test</div>
+	
+	<div class="idate">Test</div>
 </div>
 </body>
 </html>
